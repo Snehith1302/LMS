cd678a1b7be49aaf1b35248eebc42a17f165ba97
Anuka Snehith Reddy <snehithanuka2006@gmail.com>
2026-09-21 15:13:28 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express=require("express")
+const connectDB = require("./config/db")
+const dotenv=require("dotenv")
+const dns=require("dns")
+const courseRoute = require("./routes/courseRoute")
 const app=express()
 
-app.get("/welecome",(req,res)=>{
-    res.json("Welcome")
+dotenv.config()
+dns.setServers(["1.1.1.1","8.8.8.8"])
+
+app.use("/api/courses",courseRoute)
+
+// app.get("/welcome",(req,res)=>{
+//     res.send("Welcome")
+// })
+connectDB()
+
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
-
-
